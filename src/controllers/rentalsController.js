e13036a8101382444040e69c3e98f244b0c25a59
vcsil <vcsilva@poli.ufrj.br>
2022-07-31 20:33:59 -0300

    Inserir um aluguel

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -1,6 +1,8 @@
+import dayjs from "dayjs";
+
 import connection from "../database/database.js";
 
-export default async function listaAlugueis(req, res) {
+export async function listaAlugueis(req, res) {
     const { customerId, gameId } = req.query;
 
     const params = [];
@@ -38,3 +40,42 @@ export default async function listaAlugueis(req, res) {
         res.sendStatus(500);
     }
 }
+
+export async function inserirAluguel(req, res) {
+    const { customerId, gameId, daysRented } = req.body;
+
+    const rentDate = dayjs(Date.now()).format("YYYY-MM-DD"); // Data atual no momento da inserção
+    const returnDate = null;
+    const delayFee = null;
+
+    try {
+        // Pegando preço do jogo por dia
+        const { rows: game } = await connection.query(
+            `SELECT * FROM games WHERE id=$1`,
+            [gameId]
+        );
+        const { pricePerDay } = game;
+        const originalPrice =
+            parseInt(pricePerDay, 10) * parseInt(daysRented, 10);
+
+        await connection.query(
+            `INSERT INTO rentals (customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee) 
+             VALUES ($1, $2, $3, $4, $5, $6, $7);`,
+            [
+                customerId,
+                gameId,
+                rentDate,
+                daysRented,
+                returnDate,
+                originalPrice,
+                delayFee,
+            ]
+        );
+
+        res.sendStatus(201);
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+}
+// [ customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee ]
--- src/routes/rentalsRouter.js
+++ src/routes/rentalsRouter.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
-import listaAlugueis from "../controllers/rentalsController.js";
+import {
+    listaAlugueis,
+    inserirAluguel,
+} from "../controllers/rentalsController.js";
+import inserirAluguelSchemaValidationMiddleware from "../middlewares/inserirAluguelMiddleware.js";
 
 const rentalsRouter = Router();
 
 rentalsRouter.get("/rentals", listaAlugueis);
+rentalsRouter.post(
+    "/rentals",
+    inserirAluguelSchemaValidationMiddleware,
+    inserirAluguel
+);
 
 export default rentalsRouter;
